4043c64ca448b74f1072aef396ae06211b41708f
chen <hipolymer27@gmail.com>
2022-04-15 09:26:06 +0800

    home style 修改

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,20 @@ import React from "react";
 const Home = () => {
   return (
     <div className="home">
-      <h1>Local Login System</h1>
-      <p>
-        Project includes React, MongoDB Atlas, Passport.js, Authentication, JWT
-        and other examples.
-      </p>
-      <a href="https://github.com/yu8653" target="_blank" rel="noreferrer">
-        Look how it works.
-      </a>
+      <div className="card">
+        <h1>Local Login System</h1>
+        <p>
+          Project includes React, MongoDB Atlas, Passport.js, Authentication,
+          JWT and other examples.
+        </p>
+        <a
+          href="https://github.com/yu8653/post-app-frontend"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Look how it works.
+        </a>
+      </div>
     </div>
   );
 };
